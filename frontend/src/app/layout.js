617dcb14b2f687ca73b5f3a52e1950a2d499cbfc
onyekachii <emegwaonyeka@gmail.com>
2024-03-20 20:19:20 +0100

    fixed errors

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Inter, Roboto_Slab } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto_Slab = Roboto_Slab({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto_Slab.className}>{children}</body>
     </html>
   );
 }
---
+++ frontend/src/components/profileComponents/editProfile.jsx
@@ -0,0 +1,99 @@
+"use client";
+import Image from "next/image";
+import React, { useState, useRef } from "react";
+import Edit from "../../assets/edit.svg";
+import EarlyAdopter from "../../assets/early-adopter.svg";
+import { Formik, Form, Field } from "formik";
+import * as Yup from 'yup'
+
+const EditProfile = () => {
+  const [selectedImage, setSelectedImage] = useState("");
+
+  const fileInputRef = useRef(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        setSelectedImage(reader.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleUploadButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const initialValue = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    bio: "",
+    interest: {},
+    socials: {},
+  };
+
+  const validationchema = Yup.object().shape({
+    firstName: Yup.string().required("First name is required"),
+    lastName: Yup.string().required("Last name is required"),
+    email: Yup.string().required("email is required"),
+  });
+
+  return (
+    <div>
+      <div>
+        <div className="w-[115px] h-[115px] bg-slate-500 relative rounded-full">
+          {/* {selectedImage && ( */}
+          <Image
+            src={selectedImage === "" ? EarlyAdopter : selectedImage}
+            alt="profile picture"
+            width={200}
+            height={200}
+            className="w-full h-full rounded-full bg-cover"
+          />
+          {/* )} */}
+          <div
+            className="bg-white p-[10px] rounded-full z-20 absolute -right-3 shadow-md top-[70px] cursor-pointer "
+            onClick={handleUploadButtonClick}
+          >
+            <Image src={Edit} alt="Edit image" className=" w-5" />
+          </div>
+        </div>
+        <input
+          name="profileImageDoc"
+          type="file"
+          capture="environment"
+          className="hidden"
+          accept="image/*"
+          ref={fileInputRef}
+          onChange={handleImageChange}
+        />
+      </div>
+      <Formik
+        initialValues={initialValue}
+        onSubmit={() => {}}
+        validationSchema={validationchema}
+      >
+        {({ isValid, handleSubmit, values, dirty, setFieldValue }) => (
+          <Form>
+            <div>
+              <p>First Name</p>
+              <Field name="firstName" />
+            </div>
+            <div>
+              <p>Last Name</p>
+              <Field name="lastName" />
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default EditProfile;
