637ca7e5e2948b9bdf914c0b445391e29550ecb7
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-03-20 18:58:57 +0100

    Update doc.routes.ts

--- backened/src/routes/doc.routes.ts
+++ backened/src/routes/doc.routes.ts
@@ -2,7 +2,7 @@ import express, {Request, Response} from "express";
 const router = express.Router()
 
 router.get("/", async(req: Request, res: Response) => {
-    res.redirect("https://go.postman.co/workspace/f596ac10-e535-40ee-8089-40e5e6bfc0ab/collection/22416364-c1c74f71-d557-41b3-b138-c3a09ec68aea")
+    res.redirect("https://documenter.getpostman.com/view/22416364/2sA358eRhE")
 })
 
 export default router
