eb92881b3aa203ca03216b746adf5c21cfcf5d25
Adaobi-Chuks <chuksaginamada@gmail.com>
2024-03-20 06:26:08 -0800

    Docs: delete previous docs link

--- backened/src/routes/doc.routes.ts
+++ backened/src/routes/doc.routes.ts
@@ -2,7 +2,7 @@ import express, {Request, Response} from "express";
 const router = express.Router()
 
 router.get("/", async(req: Request, res: Response) => {
-    res.redirect("https://documenter.getpostman.com/view/22416364/2s9YsNeAfy")
+    res.redirect("")
 })
 
 export default router
