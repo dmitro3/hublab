6661c796a8779c331a0f46fc70e0d11cba447925
Adaobi-Chuks <chuksaginamada@gmail.com>
2024-03-20 06:18:33 -0800

    Feat: Update profile schema to fit model

--- backened/src/schemas/profile.schemas.ts
+++ backened/src/schemas/profile.schemas.ts
@@ -1,4 +1,5 @@
 import Joi from "joi";
+import { INTERESTS } from "../configs/constants.configs";
 
 const createSchema = Joi.object({
     _id: Joi.string().required().trim(),
@@ -7,31 +8,22 @@ const createSchema = Joi.object({
     email: Joi.string().email().optional().lowercase().trim(),
     imageUrl: Joi.string().optional().trim(),
     bio: Joi.string().optional().trim(),
-    phoneNumber: Joi.string().optional().trim(),
-    website: Joi.string().optional().trim(),
-    powUrl: Joi.string().optional().trim(),
+    interests: Joi.array().items(Joi.string().valid(...INTERESTS)).optional(),
+    socials: Joi.object({
+        twitter: Joi.string(),
+        linkedIn: Joi.string(),
+        discord: Joi.string(),
+        gitHub: Joi.string(),
+        instagram: Joi.string(),
+        website: Joi.string()
+    }),
+    points: Joi.object({
+        totalPoints: Joi.number().integer().min(0).optional(),
+        referalPoints: Joi.number().integer().min(0).optional(),
+        rewardPoints: Joi.number().integer().min(0).optional()
+    })
 });
 
 export {
     createSchema
 }
-// _id: string;
-// firstName: string;
-// lastName: string;
-// email: string;
-// imageUrl: string;
-// bio: string;
-// interests: [];
-// socials: {
-//     twitter: string;
-//     linkedIn: string;
-//     discord: string;
-//     gitHub: string;
-//     instagram: string;
-//     website: string;
-// };
-// points: {
-//     totalPoints: number;
-//     referalPoints: number;
-//     rewardPoints: number;
-// }
