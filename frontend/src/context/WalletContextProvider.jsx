5755fbaf605d07b1b6c5e436416a8b7da0c5990a
Donatus Prince <donatusprinc@.com>
2024-03-21 16:15:02 +0100

    solana wallet adapter fix

--- frontend/src/context/WalletContextProvider.jsx
+++ frontend/src/context/WalletContextProvider.jsx
@@ -8,7 +8,7 @@ import {
     TorusWalletAdapter
 } from '@solana/wallet-adapter-wallets'
 import * as web3 from '@solana/web3.js';
-require('@solana/wallet-adapter-react-ui/styles.css');
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 const WalletContextProvider = ({ children }) => {
 
