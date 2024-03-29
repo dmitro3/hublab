37d9d6ff002dfbe2f10731666b204598957cb6b4
onyekachii <emegwaonyeka@gmail.com>
2024-03-29 12:44:42 +0100

    added reward ui

--- frontend/src/app/campaign/page.jsx
+++ frontend/src/app/campaign/page.jsx
@@ -7,10 +7,10 @@ import React from "react";
 const page = () => {
   return (
     <>
-      <Start />
+      {/* <Start /> */}
       {/* <InputCampaign/> */}
       {/* <Criterion/> */}
-      {/* <Reward/> */}
+      <Reward/>
     </>
   );
 };
