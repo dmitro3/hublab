607d003132e736c0a90eb055c232e07e99064747
Donatus Prince <donatusprinc@.com>
2024-03-30 07:39:29 +0100

    soulbound token integrate

--- frontend/src/utils/verxioToken.ts
+++ frontend/src/utils/verxioToken.ts
@@ -5,44 +5,41 @@ import {
     Keypair,
     SystemProgram,
     Transaction,
-    LAMPORTS_PER_SOL,
-    PublicKey
+
 } from '@solana/web3.js';
 import {
     ExtensionType,
     TOKEN_2022_PROGRAM_ID,
     createInitializeMintInstruction,
+    createInitializeMetadataPointerInstruction,
     createInitializeNonTransferableMintInstruction,
     getMintLen,
-    mintTo,
-    createAccount,
-    transfer,
-    burn,
-    closeAccount,
+    LENGTH_SIZE,
+    TYPE_SIZE,
 } from '@solana/spl-token';
+import { createInitializeInstruction, pack, TokenMetadata } from '@solana/spl-token-metadata';
 import bs58 from 'bs58';
 
 
 (async () => {
     const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
-    // const secretKeyStr = process.env.NEXT_PUBLIC_VALUE;
-    // // const secretKeyBuffer = Buffer.from(bs58.decode(secretKeyStr)); // Decode the secret key
-    
     const secretKey = bs58.decode("4xRBSuyXwbKK7jwF1hV156L1AzH4h6n2sKsYhABTpKwdaKDqCg785XFreUmGtDfRG53SnRw9KtcFnf22doSTFqRt");
-    const signer = Keypair.fromSecretKey(secretKey)
-    const payer = signer;
+    const payer = Keypair.fromSecretKey(secretKey);
 
-    // const payer = Keypair.generate();
-    // const airdropSignature = await connection.requestAirdrop(payer.publicKey, 2 * LAMPORTS_PER_SOL);
-    // await connection.confirmTransaction({ signature: airdropSignature, ...(await connection.getLatestBlockhash()) });
-
-    const mintAuthority = Keypair.generate();
     const decimals = 9;
-
     const mintKeypair = Keypair.generate();
     const mint = mintKeypair.publicKey;
-    const mintLen = getMintLen([ExtensionType.NonTransferable]);
-    const lamports = await connection.getMinimumBalanceForRentExemption(mintLen);
+    const mintLen = getMintLen([ExtensionType.NonTransferable, ExtensionType.MetadataPointer]);
+    const metadata: TokenMetadata = {
+        mint: mint,
+        name: 'Verxio sToken',
+        symbol: 'sVERXIO',
+        uri: 'https://raw.githubusercontent.com/Axio-Lab/hublab/dev/frontend/frontend/src/assets/verxiobg.png',
+        additionalMetadata: [['new-field', 'new-value']],
+      };
+    
+    const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metadata).length;
+    const lamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataLen);
 
     const transaction = new Transaction().add(
         SystemProgram.createAccount({
@@ -52,8 +49,20 @@ import bs58 from 'bs58';
             lamports,
             programId: TOKEN_2022_PROGRAM_ID,
         }),
+        
+        createInitializeMetadataPointerInstruction(mint, payer.publicKey, mint, TOKEN_2022_PROGRAM_ID),
         createInitializeNonTransferableMintInstruction(mint, TOKEN_2022_PROGRAM_ID),
-        createInitializeMintInstruction(mint, decimals, mintAuthority.publicKey, null, TOKEN_2022_PROGRAM_ID)
+        createInitializeMintInstruction(mint, decimals, payer.publicKey, null, TOKEN_2022_PROGRAM_ID),
+        createInitializeInstruction({
+            programId: TOKEN_2022_PROGRAM_ID,
+            mint: mint,
+            metadata: mint,
+            name: metadata.name,
+            symbol: metadata.symbol,
+            uri: metadata.uri,
+            mintAuthority: payer.publicKey,
+            updateAuthority: payer.publicKey,
+          }),
     );
 
     const transactionSignature  = await sendAndConfirmTransaction(connection, transaction, [payer, mintKeypair], undefined);
@@ -61,32 +70,5 @@ import bs58 from 'bs58';
         "\nCreate Mint Account:",
         `https://solana.fm/tx/${transactionSignature}?cluster=devnet-solana`,
       )
-
-      const sourceTokenAccount = await createAccount(
-        connection,
-        payer, // Payer to create Token Account
-        mint, // Mint Account address
-        payer.publicKey, // Token Account owner
-        undefined, // Optional keypair, default to Associated Token Account
-        undefined, // Confirmation options
-        TOKEN_2022_PROGRAM_ID, // Token Extension Program ID
-      );
-
-    const txn = await mintTo(
-        connection,
-        payer, // Transaction fee payer
-        mint, // Mint Account address
-        sourceTokenAccount, // Mint to
-        mintAuthority, // Mint Authority address
-        100, // Amount
-        undefined, // Additional signers
-        undefined, // Confirmation options
-        TOKEN_2022_PROGRAM_ID, // Token Extension Program ID
-      );
-      
-      console.log(
-        "\nMint Tokens:",
-        `https://solana.fm/tx/${txn}?cluster=devnet-solana`,
-      );
 
 })();
