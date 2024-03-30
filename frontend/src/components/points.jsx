238d6cb88a16d00f9b2c617f9dc668b84c0d5eb6
Donatus Prince <donatusprinc@.com>
2024-03-30 11:14:46 +0100

    verxio badge

--- frontend/src/components/points.jsx
+++ frontend/src/components/points.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react'
-import VerxioCoin from "../assets/verxioCoin.svg";
+import VerxioCoinBadge from "../assets/verxioCoin.svg";
 
 const Points = () => {
   return (
@@ -9,7 +9,7 @@ const Points = () => {
       <div className="bg-[#ADEF00] p-5 rounded-2xl border border-[#486006] shadow-xl">
         <p className="text-[20px] font-medium mb-3">Balance</p>
         <div className="flex gap-2 xl:gap-4 items-center border rounded-xl px-4 xl:px-9 py- border-[#486006]">
-          <Image alt="Verxio Point Icon" src={VerxioCoin} className="w-10 mt-1" />
+          <Image alt="Verxio Point Icon" src={VerxioCoinBadge} className="w-10 mt-1" />
           <p className="font-semibold text-[20px] xl:text-[30px]">0</p>
         </div>
         <div className="flex justify-end mt-3 lg:mt-4">
