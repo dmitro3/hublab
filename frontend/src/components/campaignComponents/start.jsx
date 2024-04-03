3f4218352583d17ed6d95631beb610200fbbc437
Donatus Prince <donatusprinc@.com>
2024-04-03 21:09:30 +0100

    reward page fix

--- frontend/src/components/campaignComponents/start.jsx
+++ frontend/src/components/campaignComponents/start.jsx
@@ -12,6 +12,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setStart } from "@/store/slices/statesSlice";
 import { root } from "@/store/store";
 import Upload from "@/providers/cloudinaryProvider";
+import Tiptap from "../tiptap";
 
 const Start = () => {
   const [selectedImage, setSelectedImage] = useState("");
@@ -29,29 +30,35 @@ const Start = () => {
 
   const handleImageChange = (event) => {
     const file = event.target.files[0];
+    console.log("file", file);
 
     if (file) {
       const reader = new FileReader();
+      console.log("reader", reader);
 
       reader.onloadend = () => {
         setSelectedImage(reader.result);
       };
 
-      reader.readAsDataURL(file);
+      const data =reader.readAsDataURL(file);
+      console.log(data)
     }
   };
+  console.log(selectedImage);
 
   const handleUploadButtonClick = () => {
     fileInputRef.current.click();
   };
-
-
 
   const handleDateChange = (date) => {
     const formattedDate = dayjs(date).format("DD/MM/YYYY");
     // setSelectedDate(formattedDate)
     console.log(formattedDate);
   };
+
+    const handleDescriptionChange = (newContent) => {
+      setDescription(newContent);
+    };
 
   const initialValues = {
     title: start?.title || "",
@@ -81,29 +88,39 @@ const Start = () => {
                 <span className="mr-3 text-">*</span>Cover Banner
               </p>
               <div className="w-[65%]">
-                {/* <div className="px-28 py-24 rounded-lg border border-primary border-dashed bg-[#E7E7F9]">
-                  <div className="border rounded-lg px-2 py-1 border-[#0D0E32] ">
-                    <div className="flex items-center gap-2 justify-center">
-                      <Image alt="upload" src={UploadIcon} />
-                      <button
-                        className="text-[14px]"
-                        onClick={handleUploadButtonClick}
-                      >
-                        Upload Image
-                      </button>
-                    </div>
-                    <input
-                      name="profileImageDoc"
-                      type="file"
-                      capture="environment"
-                      className="hidden"
-                      accept="image/*"
-                      ref={fileInputRef}
-                      onChange={handleImageChange}
+                <div className=" rounded-lg border border-primary border-dashed bg-[#E7E7F9]">
+                  {selectedImage ? (
+                    <Image
+                      src={selectedImage}
+                      alt="cover Banner"
+                      className="w-full h-full bg-cover"
+                      width={500}
+                      height={400}
                     />
-                  </div>
-                </div> */}
-                <Upload/>
+                  ) : (
+                    <div className="mx-28 my-24 border rounded-lg px-2 py-1 border-[#0D0E32] ">
+                      <div className="flex items-center gap-2 justify-center">
+                        <Image alt="upload" src={UploadIcon} />
+                        <button
+                          className="text-[14px]"
+                          onClick={handleUploadButtonClick}
+                        >
+                          Upload Image
+                        </button>
+                      </div>
+                      <input
+                        name="profileImageDoc"
+                        type="file"
+                        capture="environment"
+                        className="hidden"
+                        accept="image/*"
+                        ref={fileInputRef}
+                        onChange={handleImageChange}
+                      />
+                    </div>
+                  )}
+                </div>
+                {/* <Upload/> */}
                 <div className="flex justify-between items-center text-[13px] mt-2">
                   <p>PNG / SVG / JPEG / 120*804</p>
                   <p>Max 24MB</p>
@@ -115,14 +132,15 @@ const Start = () => {
               <p className="font-semibold text-[24px] mb-5">
                 <span className="mr-3 text-">*</span>Description
               </p>
-              <div className="h-[150px] border  bg-transparent font-normal text-[14px] rounded-lg w-full border-[#0D0E32]">
+              {/* <div className="h-[150px] border  bg-transparent font-normal text-[14px] rounded-lg w-full border-[#0D0E32]">
                 <Field
                   className="w-full h-full min-h-full max-h-[150px] bg-transparent px-5 py-1 outline-none border"
                   name="description"
                   as="textarea"
                   placeholder="Enter a name"
                 />
-              </div>
+              </div> */}
+              <Tiptap onChange={handleDescriptionChange}/>
               {/* <RichTextEditor value={value} onChange={handleOnChange} /> */}
             </div>
 
